e3054ad90932fcefc84934c5f733b9de8a97f458
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-10-28 12:41:53 -0400

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,21 @@ fn main() {
     // Sets a random number between 1-100 as answer
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is {}.", secret_number);
 
     // Allows player to guess until number is found
     loop {
-        println!("Please in put your guess below:");
+        println!("Please input your guess below:");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line!");
-        let guess: i32 = guess.trim().parse()
-            .expect("Please type a number!");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
