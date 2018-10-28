41080d6217f65625c63de3e7223900c8df2f9ef6
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-10-28 12:30:27 -0400

    Fixed spacing and added comments

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-
+    // Sets a random number between 1-100 as answer
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}.", secret_number);
 
+    // Allows player to guess until number is found
     loop {
         println!("Please in put your guess below:");
         let mut guess = String::new();
